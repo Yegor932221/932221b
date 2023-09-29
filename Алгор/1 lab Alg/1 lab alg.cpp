f6b8be1dcb7abf0f9b68ddd341261ce1827f9b34
Acer <Acer@DESKTOP-IJBKLU0.local>
2023-09-29 14:59:10 +0700

    Добавил решение через 1 эвристику

--- Алгор/1 lab Alg/1 lab alg.cpp
+++ Алгор/1 lab Alg/1 lab alg.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
+#include <string.h>
 #include <time.h>
+#include<chrono>
 void InputMatrD(int** matr, int m, int n);
 void OutputMatrD(int** matr, int m, int n);
 void Swap(int* a, int b, int c);
 int CostWay(int** matr, int* rearrangement, int numberCities);
 void RandomMatr(int** matr, int n);
+void Weist(int** matr, int* weist, int numberCities);
+bool Cycl(int** ways,int** matr, int k, int numb);
 
 int main() {	
-	int start, numberCities, i, j, maxJ, maxI, k, cost = 0, lowerCost = 0, full=1;
+	int first, numberCities, i, j, maxJ, maxI, k=0, cost = 0, lowerCost = 0, full=1;
 	int* rearrangement, * cheapest;
 	int** matr;
 	std::cout << "Enter number of Citiesgg:" << std::endl;
 	std::cin >> numberCities;
 	std::cout << "Cite start" << std::endl;
-	std::cin >> start;
+	std::cin >> first;
 
 	matr = new int* [numberCities];
 	for (i = 0; i < numberCities; i++)
@@ -26,22 +30,23 @@ int main() {
 	std::cout << "Output matr:" << std::endl;
 	OutputMatrD(matr, numberCities, numberCities);
 
-	rearrangement[0] = start;
-	rearrangement[numberCities] = start;
-
+	auto start = std::chrono::high_resolution_clock::now();
+	rearrangement[0] = first;
+	rearrangement[numberCities] = first;
+// первая перестановка
 	for (i = 1, j = 1; i < numberCities; i++) {
-		if (j == start) j++;
+		if (j == first) j++;
 		rearrangement[i] = j;
 		cheapest[i] = rearrangement[i];
 		j++;
 	}
-
+	cost = CostWay(matr, rearrangement, numberCities);
 	std::cout << std::endl<<"0. ";
 	for (i = 0; i < numberCities + 1; i++) std::cout << rearrangement[i] << " ";
-	std::cout << std::endl;
-	lowerCost = CostWay(matr, rearrangement, numberCities);
+	std::cout << std::endl << "cost." << k << ": " << cost << std::endl;
+	lowerCost = cost;
 
-	for (i = 1; i < numberCities; i++)full = full * i; 
+	for (i = 1; i < numberCities; i++)full = full * i; //начало полного перебора
 
 		for (k = 1; k < full; k++) {
 			maxI = -1; maxJ=-1;
@@ -65,32 +70,109 @@ int main() {
 				Swap(rearrangement, i, j);
 				i++; j--;
 			}
-			
+			//следующая перестановка получена
 			std::cout << k << ". ";
 			for (i = 0; i < numberCities + 1; i++) std::cout << rearrangement[i] << " ";
-			std::cout << std::endl;
 			cost = CostWay(matr, rearrangement, numberCities);
+
+			std::cout << std::endl << "cost." << k << ": " << cost<<std::endl;
 
 				if (cost < lowerCost) {
 					lowerCost = cost;
-					for (j = 1; j < numberCities; j++)
+					for (j = 0; j < numberCities+1; j++)
 						cheapest[j] = rearrangement[j];
 				}
-			}
-		
-
-
-
-	lowerCost = CostWay(matr, rearrangement, numberCities);
-	std::cout << std::endl;
-	for (i = 0; i < numberCities + 1; i++) std::cout << rearrangement[i]<< " ";
+		}
+		auto end = std::chrono::steady_clock::now();
+		float time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	std::cout << std::endl<<"Time1:"<<time<<std::endl;
+	for (i = 0; i < numberCities + 1; i++) std::cout << cheapest[i]<< " ";
 	std::cout<<std::endl<<"lowerCost: " << lowerCost;
 	std::cout <<std::endl<< "Enter number of Cities: " <<numberCities << std::endl;
-	std::cout << "Cite start: " <<start << std::endl;
+	std::cout << "Cite start: " <<first << std::endl;
+
+
+	OutputMatrD(matr, numberCities, numberCities);
+	std::cout << "\n";
+	int min,max=0,o=0,ki,kj;
+	int cost2 = 0;
+	int** shortest;
+	shortest = new int*[3];
+	for (i = 0; i < 3; i++) {
+		shortest[i] = new int[numberCities];
+	}
+
+	for (i = 0; i < numberCities; i++)
+		for (j = 0; j < numberCities; j++) {
+			if (matr[i][j] > max)max = matr[i][j];
+		}
+	start = std::chrono::high_resolution_clock::now();
+	for (k = 0; k < numberCities; k++) {
+
+		min = max;
+		shortest[0][k] = 0;
+		shortest[1][k] = 0;
+		shortest[2][k] = 0;
+
+		for (i = 0; i < numberCities; i++)
+			for (j = 0; j < numberCities; j++) {
+				if (matr[i][j] != 0 && min > matr[i][j]) {
+					shortest[0][k] = i;
+					shortest[1][k] = j;
+					shortest[2][k] = matr[i][j];
+					kj = j;
+					ki = i;
+					if (Cycl(shortest, matr, k, numberCities) == 1) {
+						min = matr[i][j];
+						kj = j;
+						ki = i;
+						shortest[2][k] = matr[i][j];
+					}
+					else matr[i][j] = 0;
+				}
+			}
+		shortest[0][k] = ki;
+		shortest[1][k] = kj;
+		for (i = 0,j=0; i < numberCities && j<numberCities;j++, i++)
+		{
+			matr[ki][i] = 0;
+			matr[j][kj] = 0;
+		}
+		std::cout << "\n" << "min: " << min << "\n";
+		OutputMatrD(matr, numberCities, numberCities);
+		cost2 += shortest[2][k];
+	}
+	for(i = 0; i <numberCities;i++)
+	{
+		std::cout << "(" << shortest[0][i] << "-" << shortest[1][i] << ")\n";
+	}
+	end = std::chrono::high_resolution_clock::now();
+	time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	std::cout << "Cost2: " << cost2;
+	std::cout << "\nTime2: " << time;
+	std::cout << "\nQuality percentage:" << (lowerCost*100/ cost2) << "%";
+	for (i = 0; i < 3; i++) delete[]shortest[i];
+	delete[] shortest;
 	for (i = 0; i < numberCities; i++) delete[]matr[i];
 	delete[] matr;
 	delete[] cheapest;
 	delete[] rearrangement;
+}
+
+
+
+
+
+
+
+void Weist(int** matr,int* weist, int numberCities) {
+	for (int i = 0; i < numberCities; i++) {
+		int sum = 0;
+		for (int j = 0; j < numberCities; j++) {
+			sum += matr[i][j];
+		}
+		weist[i] = sum;
+	}
 }
 
 void InputMatrD(int** matr, int m, int n)
@@ -129,8 +211,29 @@ void OutputMatrD(int** matr, int m, int n)
 		printf("\n");
 	}
 }
+
 void Swap(int* a, int b, int c) {
 	int d = a[b];
 	a[b] = a[c];
 	a[c] = d;
  }
+
+bool Cycl(int** ways, int** matr, int k ,int numb) {
+	if (k == numb) return 1;
+	int g=0,d=k;
+	/*int y;
+	for ( y = 0; y < numb; y++) {
+		if (matr[ways[1][k]][y] != 0) break;
+	}
+	if (y == numb)return 0;*/
+	for (int i = 0; i < k; i++) {
+		if (ways[1][d] == ways[0][i]) { 
+			if (ways[1][i] == ways[0][d]) return 0;
+			d = i;
+			i = 0; 
+			g++; 
+		}
+		if (g == numb) return 0;
+	}
+	return 1;
+}
