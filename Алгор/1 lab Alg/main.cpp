ae63297c55a7495d7574502e9eb59c3697e0846d
Acer <Acer@DESKTOP-IJBKLU0.local>
2023-10-06 12:29:42 +0700

    Отмена правок из 7922ea4

--- Алгор/1 lab Alg/main.cpp
+++ Алгор/1 lab Alg/main.cpp
@@ -22,7 +22,8 @@ int main() {
 	std::cin >> first;
 
 	matr = new int* [numberCities];
-	for (i = 0; i < numberCities; i++) matr[i] = new int[numberCities];
+	for (i = 0; i < numberCities; i++)
+		matr[i] = new int[numberCities];
 
 	cheapest = new int[numberCities + 1];
 
@@ -67,7 +68,9 @@ int main() {
 void Weist(int** matr, int* weist, int numberCities) {
 	for (int i = 0; i < numberCities; i++) {
 		int sum = 0;
-		for (int j = 0; j < numberCities; j++) {sum += matr[i][j];}
+		for (int j = 0; j < numberCities; j++) {
+			sum += matr[i][j];
+		}
 		weist[i] = sum;
 	}
 }
@@ -95,7 +98,8 @@ void RandomMatr(int** matr, int n)
 
 int CostWay(int** matr, int* rearrangement, int numberCities) {
 	int i, cost = 0;
-	for (i = 0; i < numberCities; i++) cost += matr[(rearrangement[i] - 1)][(rearrangement[i + 1] - 1)];
+	for (i = 0; i < numberCities; i++)
+		cost += matr[(rearrangement[i] - 1)][(rearrangement[i + 1] - 1)];
 	return cost;
 }
 
@@ -151,7 +155,9 @@ void WayBust(int numberCities, int first, int* cheapest, int** matr) {
 	for (k = 1; k < full; k++) {
 		maxI = -1; maxJ = -1;
 
-		for (i = 1; i < (numberCities - 1); i++) { if (rearrangement[i] < rearrangement[(i + 1)]) maxI = i;}
+		for (i = 1; i < (numberCities - 1); i++) {
+			if (rearrangement[i] < rearrangement[(i + 1)]) maxI = i;
+		}
 
 		if (maxI == -1) break;
 
@@ -176,7 +182,8 @@ void WayBust(int numberCities, int first, int* cheapest, int** matr) {
 
 		if (cost < lowerCost) {
 			lowerCost = cost;
-			for (j = 0; j < (numberCities + 1); j++) cheapest[j] = rearrangement[j];
+			for (j = 0; j < (numberCities + 1); j++)
+				cheapest[j] = rearrangement[j];
 		}
 
 	}
@@ -189,11 +196,14 @@ int Evr(int numberCities, int** matr) {
 	int cost2 = 0;
 	int** shortest;
 	shortest = new int* [3];
-	for (i = 0; i < 3; i++) {shortest[i] = new int[numberCities];}
+	for (i = 0; i < 3; i++) {
+		shortest[i] = new int[numberCities];
+	}
 
 	for (i = 0; i < numberCities; i++)
-		for (j = 0; j < numberCities; j++) {if (matr[i][j] > max)max = matr[i][j];}
-
+		for (j = 0; j < numberCities; j++) {
+			if (matr[i][j] > max)max = matr[i][j];
+		}
 	for (k = 0; k < numberCities; k++) {
 
 		min = max;
