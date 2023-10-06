efb21f617b32c0ec93db2ae51c3afcfc6c1d949f
Acer <Acer@DESKTOP-IJBKLU0.local>
2023-10-06 09:30:04 +0700

    Сделал поиск первого вхождения функцией

--- Алгор/2 lab Alg/main.cpp
+++ Алгор/2 lab Alg/main.cpp
@@ -4,22 +4,32 @@
 #include <iostream>  
 
 int main() {
-	int TAB[256], i, j, m, n, k;
+	int TAB[256],i,m;
 	std::string text = "std::move_iterator is an iterator adaptor which behaves exactly like the underlying iterator", searchText = "tor";
-	n = text.length() ; m =searchText.length();
+	m = searchText.length();
 
 	for (i = 0; i < 256; i++) TAB[i] = m;
 
 	for (i = 0; i < m - 1; i++) TAB[searchText[i]] = m - 1 - i;
 
-	for (i = m - 1, j = m - 1; i < n; i = k + TAB[text[k]]	)
+	BMsearch(TAB, text, searchText, 0, -1);
+}
+
+int BMsearch(int* TAB, std::string text, std::string searchText,int start,int end) {
+	if (end<0 || end>text.length()) end = text.length();
+
+	if (start<0 || start>text.length()) start = 0;
+
+	int k = 0, i, j,m;
+	m = searchText.length();
+	for (i = m - 1 + start, j = m - 1; i < end; i = k + TAB[text[k]])
 	{
 		for (k = i, j = m - 1; j >= 0;) {
 			if (text[k] == searchText[j]) { k = k - 1, j = j - 1; }
 			else { break; }
 		}
-		if (j < 0) {return k + 1;}
-		if (k >= n) return-1;
+		if (j < 0) { return k + 1; }
+		if (k >= end) return-1;
 	}
 
 	if (j < 0) return k + 1;
