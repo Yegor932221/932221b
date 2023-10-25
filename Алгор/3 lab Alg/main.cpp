60b1f892d932a2a47accb0e3b5fa4c7b9b5d2f6c
Acer <Acer@DESKTOP-IJBKLU0.local>
2023-10-25 12:15:20 +0700

    изменил по вашим указаниям

--- Алгор/3 lab Alg/main.cpp
+++ Алгор/3 lab Alg/main.cpp
@@ -1,44 +1,95 @@
 #include<iostream>
-void RandMasAny(int a[], int b, int c, int n);
+#include<string>
+#include <random>
+#include<time.h>
+#include<vector>
+#include<fstream>
+#include<chrono>
+void RandMasAny(int b, int c, int n, FILE* mass);
 void OutputMus(int a[], int n);
+void Shell1(int* array, int size);
+void Shell2(int* array, int size);
+void Shell3(int* array, int size);
 
-
-//3 massiv 10/-10
-int* mass_10_10thou = new int[10000], * mass_1000_10thou = new int[10000], * mass_100000_10thou = new int[10000];
-//3 massiv 1000/-1000
-int* mass_10_100th = new int[100000], * mass_1000_100th = new int[100000], * mass_100000_100th = new int[100000];
-//3 massiv 100000/-100000
-int* mass_10_mil = new int[1000000], * mass_1000_mil = new int[1000000], * mass_100000_mil = new int[1000000];
 
 int main(){
-
-	//rand 3 massiv 10000
-	RandMasAny(mass_10_10thou[10000], -10, 10, 10000);
-	RandMasAny(mass_1000_10thou[10000], -1000, 1000, 10000);
-	RandMasAny(mass_100000_10thou[10000], -100000, 100000, 10000);
+	std::vector<int> arr;
+	int i;
+	FILE* mass;
 	
-	//rand 3 massiv 100000
-	RandMasAny(mass_10_100th[100000], -10, 10, 100000);
-	RandMasAny(mass_1000_100th[100000], -1000, 1000, 100000);
-	RandMasAny(mass_100000_100th[100000], -100000, 100000, 100000);
-
-	//rand 3 massiv 1000000
-	RandMasAny(mass_10_mil[1000000], -10, 10, 1000000);
-	RandMasAny(mass_1000_mil[1000000], -1000, 1000, 1000000);
-	RandMasAny(mass_100000_mil[1000000], -100000, 100000, 1000000);
-
-	OutputMus(mass_10_10thou, 10000);
+	for (int range = 10; range <= 100'000; range *=100) {
+		for (int size = 10'000; size <= 1'000'000; size *=10) {
+			std::string name = "Shell" + std::to_string(size) + "_in_range_" + std::to_string(range) +".txt";
+			const char* file = name.c_str();
+			fopen_s(&mass,file, "r");
+			if (mass == NULL)
+			{
+				perror("error opening file");
+				getchar();
+				exit(-1);
+			}
+			for (int i = 0; i < size; i++) {
+				fgets()
+			}
+		}
+	}
+	return 0;
 }
 
 
-void RandMasAny(int &a, int b, int c, int n)
+void RandMasAny(int b, int c, int n, FILE* mass)
 {
-	for (int i = 0; i < n; i++)
-		a[i] = rand() % (c - b + 1) + b;
+	std::random_device rd;   
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(b,c);
+	for (int i = 0; i < n; i++) {
+		fprintf(mass, "%d ", dist(gen));
+	}
 }
 
-void OutputMus(int a[], int n)
+void OutputMus(int* a, int n)
 {
 	for (int i = 0; i < n; i++)
 		printf("%d  ", a[i]);
+}
+
+void Shell1(int* array, int size) {
+	for (int s = size / 2; s > 0; s /= 2) {
+		for (int i = s; i < size; ++i) {
+			for (int j = i - s; j >= 0 && array[j] > array[j + s]; j -= s) {
+				int temp = array[j];
+				array[j] = array[j + s];
+				array[j + s] = temp;
+			}
+		}
+	}
+}
+
+
+void Shell2(int* array, int size) {
+	for (int s = (size / 2)+1; s > 0; s /= 2) {
+		for (int i = s; i < size; ++i) {
+			for (int j = i - s; j >= 0 && array[j] > array[j + s]; j -= s) {
+				int temp = array[j];
+				array[j] = array[j + s];
+				array[j + s] = temp;
+			}
+		}
+	}
+}
+
+void Shell3(int* array, int size) {
+	int s = size,m;
+	for (; s > 0;) {
+		for (int i = s; i < size; ++i) {
+			for (int j = i - s; j >= 0 && array[j] > array[j + s]; j -= s) {
+				int temp = array[j];
+				array[j] = array[j + s];
+				array[j + s] = temp;
+			}
+		}
+		if (s == 1)break;
+		m = (s*5 - 1) / 11;
+		s = std::max(m, 1);
+	}
 }
