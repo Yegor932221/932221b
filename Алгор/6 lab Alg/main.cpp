18cad1c74c18e5b2200cbb0182e811785c2a8b0a
���� <@DESKTOP-HUAEP1L.local>
2023-12-19 23:03:52 +0700

    Исправил ошибки (переполнение и ошибку алгоритма)

--- Алгор/6 lab Alg/main.cpp
+++ Алгор/6 lab Alg/main.cpp
@@ -10,15 +10,16 @@ void CopyArrayFromFile(const char* name, std::vector<int>& arr);
 bool ArrayCheck(std::vector<int>& arr);
 void BitSorting(std::vector<int>& arr);
 void Breakdown(std::vector<int>& arr, int l, int r, int k);
+int Max(std::vector<int> arr);
 
 int main() {
-	std::vector<int> arr;
 	for (int range = 10; range <= 100000; range *= 100)
 	{
 		for (int size = 10000; size <= 1000000; size *= 10)
 		{
 			float time = 0;
 			FILE* mass;
+			std::vector<int> arr;
 			std::string name = "Shell" + std::to_string(size) + "_in_range_" + std::to_string(range) + ".txt";
 			const char* file = name.c_str();
 			fopen_s(&mass, file, "r");
@@ -28,13 +29,15 @@ int main() {
 				getchar();
 				exit(-1);
 			}
-			CopyArrayFromFile(file, arr);
+			CopyArrayFromFile(file,arr);
+			int max =Max(arr);
 			for (int k = 3; k != 0; k--) {
+				std::vector<int> copy = arr;
 				auto start = std::chrono::high_resolution_clock::now();
-				BitSorting(arr);
+				BitSorting(copy);
 				auto end = std::chrono::high_resolution_clock::now();
 				float time1 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-				if (!ArrayCheck(arr))
+				if (!ArrayCheck(copy))
 				{
 					std::cerr << "Don't sorted";
 					break;
@@ -52,7 +55,7 @@ int main() {
 	return 0;
 }
 
-void CopyArrayFromFile(const char* name, std::vector<int>& arr) {
+void CopyArrayFromFile(const char* name, std::vector<int>& arr){
 	std::ifstream fin(name);
 	int n;
 	if (!fin.good())
@@ -74,23 +77,19 @@ bool ArrayCheck(std::vector<int>& arr) {
 	return true;
 }
 
-void BitSorting(std::vector<int>& arr) {
-	int count = arr.size();
-	int k;
-	count << 1;
-	for ( k = 0; count != 0; k++, count<<1);
-	Breakdown(arr, 0, arr.size() - 1, k);
 
-}
 
-void Breakdown(std::vector<int>& arr, int l, int r, int k) {
-	if (l >= r || k < 0) exit;
-	int i = l;
-	int j = r;
-	while (i <= j) {
-		while (i <= j && (arr[i] << k) == 0) i++;
-		while (i <= j && (arr[i] << k) == 1) j--;
-		if (i < j) {
+void Breakdown(std::vector<int>& arr, int l, int r, int k)
+{
+	if (l >= r || k < 0) return;
+	int i = l, j = r;
+	int mask = 1 << k;
+	while (i <= j)
+	{
+		while (i <= j && (arr[i] & mask)== 0) i++;
+		while (i <= j && (arr[j] & mask)!= 0) j--;
+		if (i < j)
+		{
 			std::swap(arr[i], arr[j]);
 			i++;
 			j--;
@@ -98,4 +97,38 @@ void Breakdown(std::vector<int>& arr, int l, int r, int k) {
 	}
 	Breakdown(arr, l, j, k - 1);
 	Breakdown(arr, i, r, k - 1);
+}
+
+
+void BitSorting(std::vector<int>& arr)
+{
+	int max = Max(arr);
+	int k = 0;
+	while (max) {
+		max >>= 1;
+		k++;
+	}
+	int i = 0;
+	int j = arr.size() - 1;
+	while (i <= j)
+	{
+		while (arr[i] < 0) i++;
+		while (arr[j] >= 0) j--;
+		if (i <= j) {
+			std::swap(arr[i], arr[j]);
+			i++;
+			j--;
+		}
+	}
+	Breakdown(arr, 0, j, k);
+	Breakdown(arr, i, arr.size() - 1, k);
+}
+
+int Max(std::vector<int> arr)
+{
+	int max = arr[0];
+	for (int i = 1; i < arr.size() - 1; i++) {
+		if (arr[i] > max) max = arr[i];
+	}
+	return max;
 }
