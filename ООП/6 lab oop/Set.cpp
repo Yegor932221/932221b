f17abeef028b1bcc803e7e0cdcc9a1987a933e38
���� <@DESKTOP-HUAEP1L.local>
2024-01-11 08:43:42 +0700

    Добавил ввод/вывод через консоль

--- ООП/6 lab oop/Set.cpp
+++ ООП/6 lab oop/Set.cpp
@@ -4,18 +4,18 @@
 #include "Set.h"
 
 Set::Set()
-	:BoolVector(m_size, false)
+	:BoolVector(m_size)
 {
 }
 
 Set::Set(const char* str)
-	:BoolVector(m_size, false)
+	:BoolVector(m_size)
 {
 	for (int i = 0; i < strlen(str); ++i)
 	{
 		if ((int)str[i] < (m_size + m_shift) && (int)str[i] >= m_shift)
 		{
-			Set1InRange((int)str[i] - m_shift,1);
+			Set1InRange((int)str[i] - m_shift);
 		}
 	}
 }
@@ -28,3 +28,28 @@ Set::Set(const Set& other)
 Set::~Set()
 {}
 
+std::ostream& operator<<(std::ostream& stream, const Set& set)
+{
+	for (int i = 0; i < Set::m_size; ++i)
+	{
+		if (set[i] > 0)
+			stream << (char)(i + Set::m_shift) << "(" << i + Set::m_shift << ")" << " ";
+	}
+	stream << std::endl;
+	return stream;
+}
+
+std::istream& operator>>(std::istream& stream, Set& set)
+{
+	set.Set0All();
+	std::string str;
+	std::getline(stream, str);
+	for (int i = 0; i < str.size(); ++i)
+	{
+		if ((int)str[i] < (Set::m_size + Set::m_shift) && (int)str[i] >= Set::m_shift)
+		{
+			set.Set1InRange((int)str[i] - Set::m_shift);
+		}
+	}
+	return stream;
+}
--- ООП/6 lab oop/Set.h
+++ ООП/6 lab oop/Set.h
@@ -3,13 +3,15 @@
 #include<iostream>
 #include"C:\Users\Åãîğ\932221b\ÎÎÏ\3 lab\BoolVector.h"
 
-class Set:BoolVector
+class Set : BoolVector
 {
 public:
 	Set();
 	Set(const char* str);
 	Set(const Set& other);
 	~Set();
+	friend std::ostream& operator<<(std::ostream& stream, const Set& set);
+	friend std::istream& operator>>(std::istream& stream, Set& set);
 private:
 	static const int m_size = 95;
 	static const int m_shift = 32;
